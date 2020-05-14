1460b45353393c62f48c9788786c1dc1ea0904aa
aasirah93 <62110929+aasirah93@users.noreply.github.com>
2020-05-14 16:54:58 +0100

    API weather added

---
+++ interface.js
@@ -0,0 +1,8 @@
+function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
+}
